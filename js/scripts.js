62daa7f9dd445638a20ca8eea12b5aefb1dbf6fd
Nickales Branson <bransnic000@gmail.com>
2018-03-14 12:40:54 -0700

    Version 1 of website

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+console.log('Be like Browser.');
+
+// Light Gallery
+$('#lightgallery').lightGallery({
+	
+	download: false,
+	counter: false,
+	share: false,
+	
+});
+
+$('.blueberry').blueberry({
+	hoverpause: true
+});
